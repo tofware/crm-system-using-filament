7f8f2713b3e5a06828bca2972f8ae156ff5c2126
Tofan <tofanalin1@gmail.com>
2024-05-30 14:22:05 +0300

    Employees user invitation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\AcceptInvitation;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('signed')
+    ->get('invitation/{invitation}/accept', AcceptInvitation::class)
+    ->name('invitation.accept');
